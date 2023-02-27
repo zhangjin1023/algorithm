b407bdf30b0bc8e4d8450aaaa9b0c62e38e5f235
zhangjin27 <zhangjin27@meituan.com>
2023-02-27 21:10:30 +0800

    链表

--- src/com/rambo/algorithm/sort/QuikSort.java
+++ src/com/rambo/algorithm/sort/QuikSort.java
@@ -1,7 +1,5 @@
 package com.rambo.algorithm.sort;
 
-import java.util.Arrays;
-
 /**
  * @description: 快速排序：是一种分区交换排序算法。采用分治策略对两个子序列再分别进行快速排序，是一种递归算法。
  * 时间复杂度：
@@ -12,36 +10,45 @@ import java.util.Arrays;
  * @Author : zhang_jin
  */
 public class QuikSort {
+
     public static void main(String[] args) {
-//        int[] a = new int[]{49, 38, 65, 97, 76, 13, 27};
-        int[] a = new int[]{13, 15, 20, 6, 3, 2, 17, 50};
-        System.out.println("排序前：" + Arrays.toString(a));
-        sort(a, 0, 6);
-        System.out.println("排序后：" + Arrays.toString(a));
+        int[] arr = {5, 2, 3, 1};//直接复制数组
+//        int[] arr={8,4,5,7,1,3,6};//直接复制数组
+        quick_sort(arr, 0, arr.length - 1);
+        print(arr);
     }
 
-    static void sort(int[] a, int low, int high) {
-        int i = low;
-        int j = high;
-        int key = a[low];
-        int n = 0;// 记录排序趟次
-
-        if (low < high) {
-            while (i < j) {
-                n++;
-                while (i < j && a[j] >= key) {
-                    j--;
-                }
-                a[i] = a[j];
-                while (i < j && a[i] <= key) {
-                    i++;
-                }
-                a[j] = a[i];
-                a[i] = key;
-                System.out.println("第" + n + "次排序结果：" + Arrays.toString(a));
+    private static int get_mid(int[] arr, int left, int right) {
+        int pivot = arr[left];//自定义排序中心轴，这里把arr[left]存到pivot中去，此时arr[left]为空。pivot相当于一个中间量
+        while (left < right) {//当left与right指针相遇的时候退出循环，双指针遍历结束
+            while (arr[right] >= pivot && left < right) {
+                right--;//right指针从右往左遍历，当arr[right]>=pivot，即满足以pivot为中轴，小放左，大放右的条件时，right指针继续往右遍历。当arr[right]<pivotd的时候，把当前值arr[right]赋给空置arr[left]，此时arr[right]成了空值。
             }
-            sort(a, low, i - 1);
-            sort(a, i + 1, high);
+            arr[left] = arr[right];
+            while (arr[left] <= pivot && left < right) {
+                left++;//到left指针从左往右遍历，当arr[left]<=pivot，即满足以pivot为中轴，小放左，大放右的条件时，left指针继续往左遍历。当arr[left]>pivot的时候，把当前值arr[left]赋给空置arr[right]，此时arr[left]成了空值。
+            }
+            arr[right] = arr[left];
+        }
+        //经历了上面的循环实现了pivot为中轴，小放左，大放右的格局
+        arr[left] = pivot;//最后把存放在pivot值放回数组空arr[left]中
+        return left;//返回中轴所在的下标位置。
+    }
+
+    private static void quick_sort(int[] arr, int left, int right) {
+        if (left < right) {
+            /*将arr[left..right]均分为两部分arr[left..mid]和arr[mid+1..right]
+             * ,以pivot为中轴，小放左，大放右。这是第一步。*/
+            int mid = get_mid(arr, left, right);//接收中轴所在的下标位置。
+            quick_sort(arr, left, mid - 1);//递归地对arr[left..mid]进行快速排序，使得左子序列有序
+            quick_sort(arr, mid + 1, right);//递归地对arr[mid+1..right]进行快速排序，使得左子序列有序
+        }
+    }
+
+    public static void print(int arr[])//封装函打印函数
+    {
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k] + " ");
         }
     }
 }
